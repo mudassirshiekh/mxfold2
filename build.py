95f87d2f018ae95987712034436695fa06d9d913
Kengo SATO <satoken@mail.dendai.ac.jp>
2024-04-01 16:12:39 +0900

    build with cmake

--- build.py
+++ build.py
@@ -1,18 +1,19 @@
-from glob import glob
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from cmake_build_extension import CMakeExtension, BuildExtension
+from pathlib import Path
 
 def build(setup_kwargs):
     ext_modules = [
-        Pybind11Extension(
-            "mxfold2.interface",
-            sorted(glob("mxfold2/src/**/*.cpp", recursive=True)),
+        CMakeExtension(
+            name="mxfold2.interface",
+            install_prefix="mxfold2",
+            source_dir=str(Path(__file__).parent.joinpath("mxfold2", "src")),
+            cmake_depends_on=["pybind11"],
         ),
     ]
     setup_kwargs.update(
         {
             "ext_modules": ext_modules,
-            "cmdclass": dict(build_ext=build_ext),
+            "cmdclass": dict(build_ext=BuildExtension),
             "zip_safe": False,
         }
     )
